a83cf2fa524d6d71de3c40c7074d405cc0b0a0be
Taylor May <Catalyse@users.noreply.github.com>
2018-04-30 18:06:06 -0600

    Adding comments to the cart endpoint

--- routes/cart.js
+++ routes/cart.js
@@ -8,23 +8,25 @@ var router = express.Router();
 var general = require('../security');
 
 //User Post Requests ------------------------------------------------------------------------------------------------------
+//This endpoint allows the addition of a new cart for a specific user.
+//Before allowing the user to use the endpoint, their session token is verified.
 router.post('/add/:id', function(req, res) {
-  general.PostTokenCheck(req, res, "cart/add", "ADD", function(result) {
-    if(result == true) {
+  general.PostTokenCheck(req, res, "cart/add", "ADD", function(result) {//Validate user session token
+    if(result == true) {//If token valid
       general.pool.query("SELECT * FROM `cart` WHERE complete = 0 AND uid = " + general.mysql.escape(req.params.id), function(error, result) {
-        if(error) {
+        if(error) {//Handle sql errors
           res.send("There was an error making the query! :: " + error.message);
         }
         else {
-          if(result.length > 0) {
+          if(result.length > 0) {//If there is a result, that means there is already a cart in the db, and they cannot have more than one
             res.send("Your old cart must be deleted before creating a new one!");
           }
           else { //Correct inserting into cart 
             general.pool.query("INSERT INTO `cart` (`uid`) VALUES (" + general.mysql.escape(req.params.id) + ");", function(error, result) {
-              if(error) {
+              if(error) {//Handle sql errors
                 res.send("There was an error making the query! :: " + error.message);
               }
-              else {
+              else {//Send a 1 as a confirmation of success
                 res.send('1');
               }
             });
@@ -32,12 +34,14 @@ router.post('/add/:id', function(req, res) {
         }
       });
     }
-    else {
+    else {//If token invalid send redirect notice
       res.send("$$REDIRECT$$");
     }
   });
 });
 
+//This function can be called internally to add a cart if there isnt one
+//Refer to cart adding endpoint for more detail
 function AddCart(id, finish) {
   general.pool.query("INSERT INTO `cart` (`uid`) VALUES (" + general.mysql.escape(id) + ");", function(error, result) {
     if(error) {
@@ -49,62 +53,66 @@ function AddCart(id, finish) {
   });
 }
 
-//Contents structure:
-//ItemID%Quantity$$ItemID%Quantity$$...
+//This endpoint allows users to add an item to their cart, along with a quantity
 router.post('/item/add/:id/:quantity', function(req, res) {
-  general.PostTokenCheck(req, res, "cart/item/add", "ADD", function(result) {
+  general.PostTokenCheck(req, res, "cart/item/add", "ADD", function(result) {//Validate user session token
     if(result == true) {
+      //Get the current user
       general.pool.query("SELECT * FROM `user` WHERE username = '" + req.signedCookies.session.split("@")[0] + "';", function(error,result) {
-        if(error) {
+        if(error) {//Handle SQL errors
           res.send("There was an error making the query! :: " + error.message);
         }
         else {
-          var uid = result[0].id;
+          var uid = result[0].id;//Save user id
           general.pool.query("SELECT * FROM `cart` WHERE complete = 0 AND uid = " + uid, function(error, result) {
-            if(error) {
+            if(error) {//Handle SQL errors
               res.send("There was an error making the query! :: " + error.message);
             }
             else {
-              if(result.length > 0) {
+              if(result.length > 0) {//If the user already has a cart continue
                 var cart = result[0];
                 general.pool.query("SELECT * FROM `product` WHERE id = " + general.mysql.escape(req.params.id), function(error,result) {
-                  if(error) {
+                  if(error) {//Handle SQL errors
                     res.send("There was an error making the query! :: " + error.message);
                   }
                   else {
                     if(result[0].quantity > 0) {
+                      //Check if the cart is initialized, if not we need to start the string
+                      //Contents structure:
+                      //ItemID%Quantity$$ItemID%Quantity$$...
                       if(cart.contents == '' || cart.contents == 'null' || cart.contents == 'NULL' || cart.contents == undefined) {
                         var contents = req.params.id + "%" + req.params.quantity;
                       }
-                      else {
+                      else {//Otherwise add onto the cart string
                         var contents = cart.contents + "$$" + req.params.id + "%" + req.params.quantity;
                       }
                       general.pool.query("UPDATE `cart` SET `contents` = " + general.mysql.escape(contents) + " WHERE complete = 0 AND uid = " + uid, function(error, result) {
-                        if(error) {
+                        if(error) {//Handle SQL errors
                           res.send("There was an error making the query! :: " + error.message);
                         }
-                        else {
+                        else {//Confirm action completion
                           res.send('1');
                         }
                       });
                     }
-                    else {
+                    else {//No stock of product.
                       res.send("Error: This item is out of stock!");
                     }
                   }
                 });
               }
-              else {
+              else {//If they dont have a cart, add one
                 AddCart(uid, function(result) {
                   if(result == '1') {
                     general.pool.query("SELECT * FROM `cart` WHERE complete = 0 AND uid = " + uid, function(error, result) {
-                      if(error) {
+                      if(error) {//Handle SQL errors
                         res.send("There was an error making the query! :: " + error.message);
                       }
                       else {
+                        //See the above IF branch for comments on the rest of this.
                         var cart = result[0];
                         general.pool.query("SELECT * FROM `product` WHERE id = " + general.mysql.escape(req.params.id), function(error,result) {
-                          if(error) {
+                          if(error) {//Handle SQL errors
                             res.send("There was an error making the query! :: " + error.message);
                           }
                           else {
@@ -116,7 +124,7 @@ router.post('/item/add/:id/:quantity', function(req, res) {
                                 var contents = contents + "$$" + req.params.id + "%" + req.params.quantity;
                               }
                               general.pool.query("UPDATE `cart` SET `contents` = " + general.mysql.escape(contents) + " WHERE complete = 0 AND uid = " + uid, function(error, result) {
-                                if(error) {
+                                if(error) {//Handle SQL errors
                                   res.send("There was an error making the query! :: " + error.message);
                                 }
                                 else {
@@ -131,7 +139,7 @@ router.post('/item/add/:id/:quantity', function(req, res) {
                         });
                       }
                     });
-                  }
+                  }//This implies there was an error adding the cart
                   else {
                     res.send(result);
                   }
@@ -142,7 +150,7 @@ router.post('/item/add/:id/:quantity', function(req, res) {
         }
       });
     }
-    else {
+    else {//Unauthorized
       res.send("$$REDIRECT$$");
     }
   });
@@ -151,24 +159,26 @@ router.post('/item/add/:id/:quantity', function(req, res) {
 //Contents structure:
 //ItemID%Quantity$$ItemID%Quantity$$...
 router.delete('/item/:id', function(req, res) {
-  general.PostTokenCheck(req, res, "cart/item/add", "ADD", function(result) {
+  general.PostTokenCheck(req, res, "cart/item/add", "ADD", function(result) {//Validate user auth
     if(result == true) {
+      //Find user id
       general.pool.query("SELECT * FROM `user` WHERE username = '" + req.signedCookies.session.split("@")[0] + "';", function(error,result) {
         if(error) {
           res.send("There was an error making the query! :: " + error.message);
         }
         else {
-          var uid = result[0].id;
+          var uid = result[0].id;//Save ID
+          //Get user cart
           general.pool.query("SELECT * FROM `cart` WHERE uid = " + uid, function(error, result) {
-            if(error) {
+            if(error) {//Handle SQL Errors
               res.send("There was an error making the query! :: " + error.message);
             }
             else {
-              if(result.length > 0) {
+              if(result.length > 0) {//If there is a cart
                 if(result[0].contents == '') {
                   res.send('There is no product with that ID in the cart.');
                 }
-                else {
+                else {//Split up the items in the cart, remove the item requested
                   var newContents = '';
                   var contents = result[0].contents.split('$$');
                   for(i = 0; i < contents.length; i++) {
@@ -181,17 +191,18 @@ router.delete('/item/:id', function(req, res) {
                       }
                     }
                   }
+                  //Update the cart string
                   general.pool.query("UPDATE `cart` SET `contents` = " + general.mysql.escape(newContents) + " WHERE uid = " + uid, function(error, result) {
                     if(error) {
                       res.send("There was an error making the query! :: " + error.message);
                     }
-                    else {
+                    else {//Confirm action completion
                       res.send('1');
                     }
                   });
                 }
               }
-              else {
+              else {//Send back to the user that there is no cart to delete items from
                 res.send('$$NORESULT$$');
               }
             }
